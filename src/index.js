f5977457df8e76926fbd427c737aa641ba66af0d
KaldUnU <82878865+KaldUnU@users.noreply.github.com>
2021-10-21 17:14:37 +0300

    first

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+var students = [{
+    "id" : 1,
+    "pib" : "Sladkova Olga",
+    "zdav" : true,
+    "group" : "RPZ 17 2/9",
+    "src": "https://orig00.deviantart.net/ee08/f/2009/073/e/d/free_red_panda_icon_100x100_by_supertuffpinkpuff.png"
+},
+{
+    "id" : 2,
+    "pib" : "Dragan Olga",
+    "zdav" : false,
+    "group" : "RPZ 17 1/9",
+    "src": "https://cdn-learn.adafruit.com/assets/assets/000/012/878/thumb100/led_strips_doge.bmp?1386611464"
+},
+{
+    "id" : 3,
+    "pib" : "Buro Olga",
+    "zdav" : true,
+    "group" : "RPZ 17 2/9",
+    "src": "https://vignette.wikia.nocookie.net/adventuretime/images/9/99/AT_Icons_100x100_Jake.jpg/revision/latest?cb=20120919222926&path-prefix=ar"
+}
+]
+
+new Vue({
+     el: '#app',
+     data: {
+         students: [],
+         search:'',
+         student: {"pib": "", zdav: false, group: ""}
+     },	 
+     mounted: function(){
+         this.students = students;
+     },
+     methods: {
+        deleteStudent(studId) {
+            this.students = this.students.filter(elem => {
+                return elem.id != studId;
+            });
+        },
+        addStudent() {
+            this.student.id = this.students.length+1;
+            this.students.push(this.student);
+        }
+     },
+  
+ });
+
